744862a92bb3d49d5eda5c904db63b1acbb8358c
Seyon Rajagopal <r_seyon@live.ca>
2020-11-10 22:09:53 -0500

    fix warning

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react';
-import Header from './components/Header'
+import React, { useEffect } from "react";
+import Header from "./components/Header";
 import "./App.css";
 import Chat from "./components/Chat";
 import Login from "./components/Login";
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-import { useStateValue } from './StateProvider';
+import {
+	BrowserRouter as Router,
+	Redirect,
+	Route,
+	Switch,
+} from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
 
-
 function App() {
-	// eslint-disable-next-line
-	const [{user}, dispatch] = useStateValue();
+	const [{ user }, dispatch] = useStateValue();
 
 	useEffect(() => {
 		auth.onAuthStateChanged((authUser) => {
@@ -26,21 +29,22 @@ function App() {
 				});
 			}
 		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	useEffect(() => {
 		const btnToggle = document.querySelector("#modeToggle i");
 		const hamburger = document.querySelector(".hamburger");
 		const rooms = document.querySelector("#rooms");
-        const theme = localStorage.getItem("theme");
+		const theme = localStorage.getItem("theme");
 
-        if (theme === "light") {
-            btnToggle.classList.add("fa-sun");
-            btnToggle.style.padding = "4px 3.8px";
-            btnToggle.classList.remove("fa-moon");
+		if (theme === "light") {
+			btnToggle.classList.add("fa-sun");
+			btnToggle.style.padding = "4px 3.8px";
+			btnToggle.classList.remove("fa-moon");
 			document.querySelector("body").classList.add(theme);
 		}
-		
+
 		hamburger.addEventListener("click", () => {
 			if (rooms.classList.contains("open")) {
 				rooms.classList.remove("open");
@@ -51,46 +55,48 @@ function App() {
 			}
 		});
 
-        btnToggle.addEventListener("click", () => {
-            if (btnToggle.classList.contains("fa-moon")) {
-                btnToggle.classList.add("fa-sun");
-                btnToggle.style.padding = "4px 3.8px";
-                btnToggle.classList.remove("fa-moon");
-                localStorage.setItem("theme", "light");
-            } else {
-                btnToggle.classList.add("fa-moon");
-                btnToggle.classList.remove("fa-sun");
-                btnToggle.style.padding = "4px 5px";
-                localStorage.clear();
-            }
-            document.querySelector("body").classList.toggle("light");
-        });
-	}, [user])
-	
+		btnToggle.addEventListener("click", () => {
+			if (btnToggle.classList.contains("fa-moon")) {
+				btnToggle.classList.add("fa-sun");
+				btnToggle.style.padding = "4px 3.8px";
+				btnToggle.classList.remove("fa-moon");
+				localStorage.setItem("theme", "light");
+			} else {
+				btnToggle.classList.add("fa-moon");
+				btnToggle.classList.remove("fa-sun");
+				btnToggle.style.padding = "4px 5px";
+				localStorage.clear();
+			}
+			document.querySelector("body").classList.toggle("light");
+		});
+	}, [user]);
+
 	return (
 		<div className="App">
-			{!user ? 
+			{!user ? (
 				<>
-					<Header/>
-					<Login/>
+					<Header />
+					<Login />
 				</>
-			: 
-			<div className="appBody">
-				<Router>
-					<Switch>
-						<Route exact path="/">
-							<Header/>
-							<Chat/>
-						</Route>
-						<Route exact path="/chat/:id" >
-							<Header/>
-							<Chat/>
-						</Route>
-						<Route><Redirect to="/"/></Route>
-					</Switch>
-				</Router>
-			</div>
-			}
+			) : (
+				<div className="appBody">
+					<Router>
+						<Switch>
+							<Route exact path="/">
+								<Header />
+								<Chat />
+							</Route>
+							<Route exact path="/chat/:id">
+								<Header />
+								<Chat />
+							</Route>
+							<Route>
+								<Redirect to="/" />
+							</Route>
+						</Switch>
+					</Router>
+				</div>
+			)}
 		</div>
 	);
 }
